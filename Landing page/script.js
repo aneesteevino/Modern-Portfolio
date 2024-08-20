a51b064335485861d8a2cef0d662ea1a827b43b4
Muhammad Anees <149869784+aneesteevino@users.noreply.github.com>
2024-08-20 21:18:15 +0500

    first commit

---
+++ Landing page/script.js
@@ -0,0 +1,26 @@
+document.getElementById("github").addEventListener("click", function() {
+    window.location.href = "https://github.com/aneesteevino";
+  });
+
+  document.getElementById("linkedIn").addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/anees-teevino-56017329a/";
+  });
+
+  document.getElementById("insta").addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/anees-teevino-56017329a/";
+  });
+  
+  document.getElementById("insta").addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/anees_teevino/";
+  });
+
+  document.getElementById("contact").addEventListener("click", function() {
+    window.location.href = "../Contact/index.html";
+});
+
+document.getElementById("Contact").addEventListener("click", function() {
+    window.location.href = "../Contact/index.html";
+});
+document.getElementById("Projects").addEventListener("click", function() {
+    window.location.href = "../Technologies/index.html";
+})
